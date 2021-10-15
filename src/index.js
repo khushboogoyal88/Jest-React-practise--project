a76f141a593d0b40cd3a9a02ea5eb55fa2bc7a1d
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-10-15 09:53:27 +0300

    Test and summary form

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SummarForm from './pages/summary/SummaryForm';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SummarForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
